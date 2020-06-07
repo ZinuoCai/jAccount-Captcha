b4b76465ac36f8a46401d72da4283bbdb8d8dfca
ZinuoCai <zinuocai@gmail.com>
2020-06-07 18:45:15 +0800

    feat: init, collect data

---
+++ download_images.py
@@ -0,0 +1,25 @@
+import os
+import requests
+
+uuid = None
+t = None
+image_count = 1000
+
+headers = {
+    'Accept': 'text/html, */*; q=0.01',
+    'Referer': '',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
+                  'Chrome/83.0.4103.61 Safari/537.36',
+}
+base_url = 'https://jaccount.sjtu.edu.cn/jaccount/captcha?uuid=%s&t=%s' % (uuid, t)
+base_dir = 'data/source_images/'
+
+if not os.path.exists(base_dir):
+    os.makedirs(base_dir)
+
+for i in range(image_count):
+    image_url = '%s%04d' % (base_url, i)
+    image_dir = '%s%04d.png' % (base_dir, i)
+    resp = requests.get(image_url, headers=headers)
+    with open(image_dir, 'wb') as f:
+        f.write(resp.content)
---
+++ process_images.py
@@ -0,0 +1,67 @@
+import cv2 as cv
+import numpy as np
+
+# label source_images size
+image_count = 250
+# output size
+output_size = 28
+
+# read lables
+with open('data/labels.csv', 'r') as f:
+    lines = f.readlines()
+
+for i in range(image_count):
+    split_label = lines[i][:-1].split(',')
+    source_filename = 'data/source_images/%04d.png' % i
+
+    color_img = cv.imread(source_filename)
+    gray_img = cv.cvtColor(color_img, cv.COLOR_BGR2GRAY)
+    height, width = gray_img.shape
+
+    blurred = cv.medianBlur(gray_img, 3)
+    kernel = np.ones((3, 3), np.uint8)
+    erosion = cv.erode(blurred, kernel, iterations=1)
+
+    sum_by_col = np.sum(erosion != 255, axis=1)
+    sum_by_row = np.sum(erosion != 255, axis=0)
+
+    top, down, left, right = 0, height - 1, 0, width - 1
+
+    while top < height:
+        if sum_by_col[top] != 0:
+            break
+        else:
+            top += 1
+
+    while down > 0:
+        if sum_by_col[top] != 0:
+            break
+        else:
+            down -= 1
+
+    while left < width:
+        if sum_by_row[left] != 0:
+            break
+        else:
+            left += 1
+
+    while right > 0:
+        if sum_by_row[right] != 0:
+            break
+        else:
+            right -= 1
+
+    label_count = len(split_label)
+    single_width = (right - left) // (label_count - 1)
+    vertical_padding = (max(output_size, height) - (down - top)) // 2
+    horizontal_padding = (max(output_size, width) - (right - left)) // 2
+
+    for j in range(1, label_count):
+        label = split_label[j]
+        filename = 'data/split_images/%s/%04d.png' % (label, i)
+        single_img = color_img[top:down, left + single_width * (j - 1):left + single_width * j, :]
+        single_img = cv.copyMakeBorder(single_img, vertical_padding, vertical_padding,
+                                       horizontal_padding, horizontal_padding,
+                                       cv.BORDER_CONSTANT, value=[255, 255, 255])
+        single_img = cv.resize(single_img, (output_size, output_size))
+        cv.imwrite(filename, single_img)
